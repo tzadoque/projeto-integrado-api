1cf250047582ddcacb99c76b2d29b844073d6bf9
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-12-05 00:47:37 -0300

    feat: Adicionando a estrutura para a documentação no swagger

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const routes = require('./routes/routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocs = require('./swagger.json');
 
 require('./database/index');
 
 const app = express();
 
 app.use(express.json());
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/v1', routes);
 
 const PORT = 3000;
