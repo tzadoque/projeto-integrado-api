49dfeb3a14f5a92b1dc3f112d06db5070798bcbd
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-12-08 23:20:43 -0300

    fix(database): Atualizando o arquivo para acessar o banco postgres
    Modificando a configuração do sequelize para acessar o banco postgres que é criado com o docker compose

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,14 @@
 module.exports = {
-  dialect: 'sqlite',
-  storage: '../database/db.sqlite',
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'root',
+  database: 'projeto_integrado_database',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    freezeTableName: true,
+  },
 };
