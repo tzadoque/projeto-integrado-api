010bc120cfe12edab4cb131056b4ec39db5baf56
Zadoque Teófilo <zadoque.dev@gmail.com>
2022-12-24 22:06:12 -0300

    refactor: adicionando extensão na importação do arquivo de configuração

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const dbConfig = require('../config/database');
+const dbConfig = require('../config/database.json');
 
 const User = require('../models/User');
 
